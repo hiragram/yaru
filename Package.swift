ad453a7f6b21eb9f59dd469f723165bb7930ae1a
Yuya Hirayama <hiragram@gmail.com>
2022-03-12 13:32:53 +0900

    Fix package swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "yaru",
+    products: [
+        .executable(name: "yaru", targets: ["yaru"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
