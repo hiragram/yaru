60acdc28923ad99ca1b9e1745460f792cfc23a0d
noppefoxwolf <noppelabs@gmail.com>
2022-03-16 20:37:37 +0900

    Use ArgumentParser

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,6 +24,7 @@ let package = Package(
             name: "yaru",
             dependencies: [
                 "Yams",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ]),
         .testTarget(
             name: "yaruTests",
---
+++ Sources/yaru/Yaru.swift
@@ -0,0 +1,73 @@
+import Foundation
+import Yams
+import ArgumentParser
+
+@main
+struct Yaru: ParsableCommand {}
+
+extension Yaru {
+    struct Init: ParsableCommand {
+        mutating func run() throws {
+            guard try Storage.default.load() == nil else {
+                throw YaruError.alreadyInitialized
+            }
+            Storage.default.initialize()
+            let emptyList = YaruList.init(items: [])
+            try Storage.default.save(yaruList: emptyList)
+        }
+    }
+    
+    struct Current: ParsableCommand {
+        mutating func run() throws {
+            if let item = try Storage.default.load()?.items.first {
+                print(item.title)
+            }
+        }
+    }
+    
+    struct Push: ParsableCommand {
+        @Argument
+        var title: String
+        
+        mutating func run() throws {
+            guard var storedList = try Storage.default.load() else {
+                throw YaruError.uninitialized
+            }
+            storedList.items.insert(.init(title: title), at: 0)
+            try Storage.default.save(yaruList: storedList)
+        }
+    }
+    
+    struct Enqueue: ParsableCommand {
+        @Argument
+        var title: String
+        
+        mutating func run() throws {
+            guard var storedList = try Storage.default.load() else {
+                throw YaruError.uninitialized
+            }
+            storedList.items.append(.init(title: title))
+            try Storage.default.save(yaruList: storedList)
+        }
+    }
+    
+    struct Next: ParsableCommand {
+        mutating func run() throws {
+            guard var storedList = try Storage.default.load() else {
+                throw YaruError.uninitialized
+            }
+            storedList.items = Array(storedList.items.dropFirst())
+            try Storage.default.save(yaruList: storedList)
+        }
+    }
+    
+    struct All: ParsableCommand {
+        mutating func run() throws {
+            if let list = try Storage.default.load() {
+                list.items.forEach { item in
+                    print(item.title)
+                }
+            }
+        }
+    }
+}
